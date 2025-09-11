9cafbcc16a17b193ad515839137390864b629076
KNODRON <juan.jimenezv08@gmail.com>
2025-09-11 18:51:36 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "osint-panel-v1";
+const urlsToCache = [
+  "index.html",
+  "manifest.json",
+  "icon-192.png",
+  "icon-512.png"
+];
+
+// Instalar y guardar en caché
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activar y limpiar cachés viejas
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Servir desde caché
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
